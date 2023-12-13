2c0ad863747f18070b31d1fa4223ddbb4fcd78e0
portfolio <sarathblu1996@gmail.com>
2023-12-13 22:40:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Home from "./components/Home";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
-<Router>
-
-<Routes>
-    {/* <Route path='/' element={<Home />} /> */}
-    <Route
-      path='/*'
-      element={
-        <>
-          <Routes>
-            <Route path='/' element={<Login />} />
-            {/* <Route path='/projects' element={<Projects />} /> */}
-            {/* <Route path='/contact' element={<Contact />} /> */}
-          </Routes>
-          {/* <Footer /> */}
-        </>
-      }
-    />
-  </Routes>
-</Router>
+    <Router>
+      <Routes>
+        <Route
+          path="/*"
+          element={
+            <>
+              <Routes>
+                <Route path="/" element={<Login />} />
+              </Routes>
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
